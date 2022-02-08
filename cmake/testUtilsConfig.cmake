8e59b93ba21ceb43409449fb2331b66586878efb
Benedict Hartmann <bene.hartmann@web.de>
2022-02-08 21:19:11 +0100

    argn instead of parsing

--- cmake/testUtilsConfig.cmake
+++ cmake/testUtilsConfig.cmake
@@ -4,10 +4,8 @@ macro(addCatchTest targetname)
         file(MAKE_DIRECTORY ${CMAKE_BINARY_DIR}/testresult)
         set(ARGS -r junit -o ${CMAKE_BINARY_DIR}/testresult/${targetname}.xml)
     endif()
-    cmake_parse_arguments(TEST_PARSE "${options}" ""
-                          "" ${ARGN})
 
-    add_executable(${targetname} ${TEST_PARSE_UNPARSED_ARGUMENTS})
+    add_executable(${targetname} ${ARGN})
     target_link_libraries(${targetname} PRIVATE Catch2::Catch2)
     target_compile_definitions(${targetname} PRIVATE CATCH_CONFIG_MAIN)
     add_test(NAME ${targetname} COMMAND ${targetname} ${ARGS})
